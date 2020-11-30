7b66ea27cc753137fbb476a5dd72195eb82f37bc
ChrisWhite12 <chris_white_12@hotmail.com>
2020-11-30 13:11:32 +1000

    updated login, register and nav components

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,19 @@ import React, {Component} from 'react'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="main_sec">
+            <h1>Welcome to the Brain Train Client Portal</h1>
+            <div className="login_card">
+                <input className="login_fields" type="text" placeholder="email"></input>
+                <input className="login_fields" type="password" placeholder="password"></input>
+                <div>
+                    <input type="checkbox"></input>
+                    <label> Remember Me</label>
+                </div>
+                <a>Forgot Password</a>
+                <input id="login_btn" type="submit" value="Log In"></input>
+                <input id="sign_up_btn" type="button" value="Sign Up"></input>
+            </div>
         </div>
     )
 }
