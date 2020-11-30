1400d31e52714d28b491e15bf22b7575d587bf15
Caitlin McCawley <caitlin.mccawley@gmail.com>
2020-11-30 15:48:38 +1000

    using yarn - deleted some stuff...dunno if that was a bad idea...also installed react-bootstrap. Started dashboard design

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,37 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+// reactstrap components
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from "reactstrap";
 
-const Dashboard = () => {
+class Dashboard extends React.Component {
+  render() {
     return (
         <div>
-            <h1>Dashboard</h1>
+            <div className="main_sec">
+            <h1>Welcome userEmail //useremail will be linked here</h1>
+            <div>
+            <Card
+                bg={Dark.toLowerCase()}
+                key={Dark}
+                text={Dark.toLowerCase() === 'light' ? 'dark' : 'white'}
+                style={{ width: '18rem' }}
+                className="mb-2"
+            >
+                    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                <CardBody>
+                    <CardTitle tag="h5">Card title</CardTitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                </CardBody>
+            </Card>
+            </div>
+            </div>
         </div>
-    )
+    );
+  }
 }
 
-export default Dashboard
+export default Dashboard;
