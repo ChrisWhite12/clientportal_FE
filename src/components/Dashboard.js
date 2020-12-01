cfa7e2e2aa06a1533ae36e7692bdf36479e44754
ChrisWhite12 <chris_white_12@hotmail.com>
2020-12-01 15:41:56 +1000

    added proxy for backend

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react'
+import {useGlobalState} from '../config/store'
 
+    
 const Dashboard = () => {
+
+    const {store} = useGlobalState()
+    const {loggedInUser} = store
+
     return (
         <div>
             <h1>Dashboard</h1>
+            <p> Hello {loggedInUser}</p>
         </div>
     )
 }
