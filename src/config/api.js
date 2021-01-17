773db63becdcac38e16f5cf7a3de14b2de694bb4
ChrisWhite12 <chris_white_12@hotmail.com>
2021-01-18 06:06:15 +1000

    updating BE url

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL: 'http://localhost:3009',
-    baseURL: (process.env.NODE_ENV='development') ? "http://localhost:3009" :"https://evening-falls-81226.herokuapp.com/",
+    baseURL: (process.env.NODE_ENV === 'development') ? "http://localhost:3009" :"https://evening-falls-81226.herokuapp.com/",
     timeout: 5000,
     withCredentials: true
 })
