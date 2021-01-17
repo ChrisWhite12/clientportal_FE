8bf89b43d779e388816f7e2b74fd603d966c5d7a
ChrisWhite12 <chris_white_12@hotmail.com>
2021-01-17 18:44:52 +1000

    added redirects

--- src/config/api.js
+++ src/config/api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export default axios.create({
     baseURL: 'http://localhost:3009',
+    // baseURL: "https://evening-falls-81226.herokuapp.com/",
     timeout: 5000,
     withCredentials: true
 })
